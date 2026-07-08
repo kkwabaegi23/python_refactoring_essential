07d29bb550fb332b826fbb5fa92920c091d948bc
Jonas Kuhlmann <J02341@uniper.energy>
2026-07-08 11:57:36 +0200

    It works!

--- comments/test/test_PowerUtils.py
+++ comments/test/test_PowerUtils.py
@@ -11,19 +11,19 @@ class TestPowerUtils(unittest.TestCase):
         self.assertEqual(PowerUtils.n(-3), 9)
 
     def test_m_single_value(self):
-        self.assertEqual(PowerUtils.m(3, 3), 9)
+        self.assertEqual(PowerUtils.sum_of_squares(3, 3), 9)
 
     def test_m_small_range(self):
-        self.assertEqual(PowerUtils.m(1, 3), 14)
+        self.assertEqual(PowerUtils.sum_of_squares(1, 3), 14)
 
     def test_m_larger_range(self):
-        self.assertEqual(PowerUtils.m(0, 3), 14)
+        self.assertEqual(PowerUtils.sum_of_squares(0, 3), 14)
 
     def test_m_negative_range(self):
-        self.assertEqual(PowerUtils.m(-2, 0), 5)
+        self.assertEqual(PowerUtils.sum_of_squares(-2, 0), 5)
 
     def test_m_larger_negative_to_positive(self):
-        self.assertEqual(PowerUtils.m(-1, 1), 2)
+        self.assertEqual(PowerUtils.sum_of_squares(-1, 1), 2)
 
 
 if __name__ == "__main__":

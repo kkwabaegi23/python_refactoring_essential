8e904c98c127064c8e0288bbe43136d8dad46de8
Andrew Stretton <andrew.stretton@elemica.com>
2026-07-08 11:14:39 +0100

    It works!

--- comments/src/PowerUtils.py
+++ comments/src/PowerUtils.py
@@ -4,14 +4,11 @@ class PowerUtils:
     def sum_of_squares(lower_bound, upper_bound):
         result = 0
 
-        # Iterate from lower to upper bound 
         for current_value in range(lower_bound, upper_bound + 1):
-            # Add square of each number in the range
             result += PowerUtils.square(current_value)
 
         return result
 
     @staticmethod
     def square(k):
-        # Return square of input
         return k * k
